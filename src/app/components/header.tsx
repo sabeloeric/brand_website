96caf275f4d6c1445570e064495a6888658c8974
Sabelo <thbsab001@myuct.ac.za>
2023-09-02 18:40:34 +0200

    Make header responsive on mobile

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import Image from 'next/image';
 import '../scss/components/header.scss';
 
 function Header() {
+  const [isSideNavOpen, setIsSideNavOpen] = useState(false);
+
+  const toggleSideNav = () => {
+    setIsSideNavOpen(!isSideNavOpen);
+  };
+
   return (
-    <nav className='header'>
+    <nav className={`header ${isSideNavOpen ? 'open' : 'closed'}`}>
       <div className='header__container'>
+        <button
+          className='header__toggle-button'
+          onClick={toggleSideNav}>
+          ☰
+        </button>
         <div className='header__figure'>
           <Image
             fill={true}
@@ -31,6 +44,26 @@ function Header() {
         <button className='header__button'>
           Let&apos;s Talk
         </button>
+      </div>
+      {/* Side Navigation Menu */}
+      <div className={`header__sidenav ${isSideNavOpen ? 'open' : 'closed'}`}>
+        <button
+          className='header__toggle-button header__toggle-button--close'
+          onClick={toggleSideNav}>
+          X
+        </button>
+        <a href='#' className='header__link'>
+          Services
+        </a>
+        <a href='#' className='header__link'>
+          Industries
+        </a>
+        <a href='#' className='header__link'>
+          Cases
+        </a>
+        <a href='#' className='header__link'>
+          Contact
+        </a>
       </div>
     </nav>
   );
